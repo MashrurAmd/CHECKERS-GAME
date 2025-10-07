a7bbf31fdca23040cc87d8270b85050148f83774
Mashrur Ahmed <mashrurahmed500@gmail.com>
2025-10-07 16:43:47 +0600

    4 DICES

--- Assets/2/DiceSpawner.cs
+++ Assets/2/DiceSpawner.cs
@@ -5,11 +5,27 @@ public class DiceSpawner : MonoBehaviour
 {
     public GameObject dicePrefab; // Assign your dice prefab here
     public Tilemap tilemap;       // Assign your tilemap here
-    public Vector3Int spawnCell = new Vector3Int(0, 0, 0); // Where to spawn
+
+    // List of starting spawn positions for multiple dice
+    private Vector3Int[] spawnPositions = new Vector3Int[]
+    {
+        new Vector3Int(-4, -4, 0),
+        new Vector3Int(-2, -4, 0),
+        new Vector3Int(0, -4, 0),
+        new Vector3Int(2, -4, 0)
+    };
 
     void Start()
     {
-        SpawnDice(spawnCell);
+        SpawnAllDice();
+    }
+
+    void SpawnAllDice()
+    {
+        foreach (var cell in spawnPositions)
+        {
+            SpawnDice(cell);
+        }
     }
 
     public void SpawnDice(Vector3Int cellPosition)
@@ -17,7 +33,7 @@ public class DiceSpawner : MonoBehaviour
         Vector3 spawnPos = tilemap.GetCellCenterWorld(cellPosition);
         GameObject dice = Instantiate(dicePrefab, spawnPos, Quaternion.identity);
 
-        // Assign Tilemap reference to dice script
+        // Assign Tilemap reference to each dice
         DiceController controller = dice.GetComponent<DiceController>();
         if (controller != null)
         {
